d663dd936e8ad70c92da4b247e369e6e0205a171
karthirajan <yashsg94@gmail.com>
2019-03-05 15:36:42 +0530

    json

--- src/test/java/steps/TestRunner.java
+++ src/test/java/steps/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features/AbiBus.feature", glue= {"steps"}, plugin= {"html:target"}, monochrome=true
+@CucumberOptions(features="src/test/resources/features/AbiBus.feature", glue= {"steps"}, plugin= {"html:target","json:src/test/java/pack/reports.json"}, monochrome=true
                                  ,dryRun=false)
 public class TestRunner {
 
